d17bebd065e3b0fd4f341dc06e55ebb4e6fc8208
nbaledio <nbaledio@ucsc.edu>
2019-02-18 19:12:53 -0800

    Pushing for unix

---
+++ asg_3grading/listmap.tcc
@@ -0,0 +1,181 @@
+// $Id: listmap.tcc,v 1.11 2018-01-25 14:19:14-08 - - $
+
+#include "listmap.h"
+#include "debug.h"
+
+//
+/////////////////////////////////////////////////////////////////
+// Operations on listmap::node.
+/////////////////////////////////////////////////////////////////
+//
+
+//
+// listmap::node::node (link*, link*, const value_type&)
+//
+template <typename Key, typename Value, class Less>
+listmap<Key,Value,Less>::node::node (node* next, node* prev,
+                                     const value_type& value):
+            link (next, prev), value (value) {
+}
+
+//
+/////////////////////////////////////////////////////////////////
+// Operations on listmap.
+/////////////////////////////////////////////////////////////////
+//
+
+//
+// listmap::~listmap() Destructor
+//
+template <typename Key, typename Value, class Less>
+listmap<Key,Value,Less>::~listmap() {
+   DEBUGF ('l', reinterpret_cast<const void*> (this));
+   node* it = head->next;
+   while(it != back){
+        erase(it);
+        it = head->next;
+   }
+}
+
+//Insert completed
+
+//
+// iterator listmap::insert (const value_type&)
+//
+template <typename Key, typename Value, class Less>
+typename listmap<Key,Value,Less>::iterator
+listmap<Key,Value,Less>::insert (const value_type& pair) {
+   DEBUGF ('l', &pair << "->" << pair);
+   //Searches for key to see if it exists and overwrites value
+   if(find(pair.first) != back){
+        find(pair.first)->second = pair.second;
+        return find(pair.first);
+   }
+   //Checks if list is empty and inserts it between head and anchor
+   if(head->next == back){
+        node* newnode = new node(back,head,pair);
+        head->next = newnode;
+        back->prev = newnode;
+        return newnode; 
+   }
+   node* it = head->next;
+   //Inserts new node lexicohraphically
+   while(it != back){
+//std::string(pair.first)<std::string(it->value.first)
+        if(less(pair.first,it->value.first)==true){
+                node* newnode = new node(it,it->prev,pair);
+                it->prev->next = newnode;
+                it->prev = newnode;
+                return newnode;
+        }
+        it = it->next;
+        if(it == back){
+                node* largest = new node(back,back->prev,pair);
+                back->prev->next = largest;
+                back->prev = largest;
+                return largest;
+        }
+   }
+   return end();
+}
+
+// Find completed
+//
+// listmap::find(const key_type&)
+//
+template <typename Key, typename Value, class Less>
+typename listmap<Key,Value,Less>::iterator
+listmap<Key,Value,Less>::find (const key_type& that) {
+  DEBUGF ('l', that);
+   node* it = head->next;
+   while(it != back){
+        if(it->value.first == that){
+                return it;
+        }
+        it = it->next;
+   }
+   return end();
+}
+
+//Erase completed
+//
+// iterator listmap::erase (iterator position)
+//
+template <typename Key, typename Value, class Less>
+typename listmap<Key,Value,Less>::iterator
+listmap<Key,Value,Less>::erase (iterator position) {
+   DEBUGF ('l', &*position);
+   position.where->prev->next = position.where->next;
+   position.where->next->prev = position.where->prev;
+   iterator it = position.where->next;
+   delete position.where;
+   return it;
+}
+
+//
+/////////////////////////////////////////////////////////////////
+// Operations on listmap::iterator.
+/////////////////////////////////////////////////////////////////
+//
+
+//
+// listmap::value_type& listmap::iterator::operator*()
+//
+template <typename Key, typename Value, class Less>
+typename listmap<Key,Value,Less>::value_type&
+listmap<Key,Value,Less>::iterator::operator*() {
+   DEBUGF ('l', where);
+   return where->value;
+}
+
+//
+// listmap::value_type* listmap::iterator::operator->()
+//
+template <typename Key, typename Value, class Less>
+typename listmap<Key,Value,Less>::value_type*
+listmap<Key,Value,Less>::iterator::operator->() {
+   DEBUGF ('l', where);
+   return &(where->value);
+}
+
+//
+// listmap::iterator& listmap::iterator::operator++()
+//
+template <typename Key, typename Value, class Less>
+typename listmap<Key,Value,Less>::iterator&
+listmap<Key,Value,Less>::iterator::operator++() {
+   DEBUGF ('l', where);
+   where = where->next;
+   return *this;
+}
+
+//
+// listmap::iterator& listmap::iterator::operator--()
+//
+template <typename Key, typename Value, class Less>
+typename listmap<Key,Value,Less>::iterator&
+listmap<Key,Value,Less>::iterator::operator--() {
+   DEBUGF ('l', where);
+   where = where->prev;
+   return *this;
+}
+
+
+//
+// bool listmap::iterator::operator== (const iterator&)
+//
+template <typename Key, typename Value, class Less>
+inline bool listmap<Key,Value,Less>::iterator::operator==
+            (const iterator& that) const {
+   return this->where == that.where;
+}
+
+//
+// bool listmap::iterator::operator!= (const iterator&)
+//
+template <typename Key, typename Value, class Less>
+inline bool listmap<Key,Value,Less>::iterator::operator!=
+            (const iterator& that) const {
+   return this->where != that.where;
+}
+
---
+++ asg_3grading/main.cpp
@@ -0,0 +1,244 @@
+// $Id: main.cpp,v 1.11 2018-01-25 14:19:29-08 - - $
+
+#include <cstdlib>
+#include <exception>
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <fstream>
+#include <sstream>
+#include <regex>
+
+using namespace std;
+
+#include "listmap.h"
+#include "xpair.h"
+#include "util.h"
+
+using str_str_map = listmap<string,string>;
+using str_str_pair = str_str_map::value_type;
+
+void scan_options (int argc, char** argv) {
+   opterr = 0;
+   for (;;) {
+      int option = getopt (argc, argv, "@:");
+      if (option == EOF) break;
+      switch (option) {
+         case '@':
+            debugflags::setflags (optarg);
+            break;
+         default:
+            complain() << "-" << char (optopt) << ": invalid option"
+                       << endl;
+            break;
+      }
+   }
+}
+
+string trimstring(string word){
+   for(unsigned int i = 0; i < word.length(); i++){
+      if(word.at(i) == 32 ){
+          word.erase(i,1);
+          i = -1;
+          continue;
+      }else{
+          break;
+      }
+   }
+   for(unsigned int i = word.length()-1; i>0; i--){
+      if(word.at(i) == 32 ){
+          word.erase(i,1);
+          i = word.length();
+          continue;
+      }else{
+          break;
+      }
+   }
+return word;
+}
+
+int main (int argc, char** argv) {
+  
+if(argc == 1){
+  goto cin_loop;
+}
+
+if(argc > 1){
+  str_str_map map;
+  fstream file;
+  std:: string line;
+  smatch match;
+  string arg;
+  string arg2;
+
+  regex comment{("^\\s*#.*$")};
+  regex key{("^\\s*[^#.*=]*[^=]$")};
+  regex key_equals{("(^\\s*[^=]+)=\\s*$")};
+  regex key_equals_value{("(^\\s*[^=]+)=(\\s*\\S.*)$")};
+  regex equals{("^\\s*=\\s*$")};
+  regex equals_value{("^\\s*=(\\s*\\S.*$)")};
+
+  //Loop to handle file inputs
+  for(int i = 1; i < argc; i++){
+  map.initialize();
+  int linenum = 0;
+  string argument = argv[i];
+  if(argument == "-"){
+        goto cin_loop;
+  }
+  file.open(argv[i]);
+  while(std::getline(file,line)){
+        linenum++;
+        cout << argv[i] << ": " << linenum << ": " << line << endl;
+        if(regex_search(line,match,comment)){
+               continue;
+        }
+        if(regex_search(line,match,key)){
+               arg = match[0];
+               arg = trimstring(arg);
+               if(map.find(arg) == map.end()){
+                        try{
+                        string error = arg;
+                        throw runtime_error(error + 
+                        ": key not found");
+                        }
+                        catch (runtime_error err)
+                        {cerr << err.what() << endl;}
+                        continue;
+               }else{
+                        cout << map.find(arg)->first << " = "
+                        << map.find(arg)->second << endl;
+               }
+        }
+        if(regex_search(line,match,key_equals)){
+               arg = match[1];
+               arg = trimstring(arg);
+               if(map.find(arg) != map.end()){
+                        map.erase(map.find(arg));
+               }
+               continue;
+        }
+        if(regex_search(line,match,key_equals_value)){
+                arg = match[1];
+                arg2 = match[2];
+                arg = trimstring(arg);
+                arg2 = trimstring(arg2);
+                cout << arg << " = " << arg2 << endl;
+                str_str_pair pair(arg,arg2);
+                map.insert(pair);
+                continue;
+        }
+        if(regex_search(line,match,equals)){
+                map.print();
+                continue;
+        }
+        if(regex_search(line,match,equals_value)){
+               arg = match[1];
+               arg = trimstring(arg);
+               map.print_value(arg);
+               continue;
+        } 
+   }
+   file.close();
+   map.~listmap();
+  }
+}
+return 0;
+cin_loop:
+  str_str_map map;
+  std:: string line;
+  smatch match;
+  string arg;
+  string arg2;
+
+  regex comment{("^\\s*#.*$")};
+  regex key{("^\\s*[^#.*=]*[^=]$")};
+  regex key_equals{("(^\\s*[^=]+)=\\s*$")};
+  regex key_equals_value{("(^\\s*[^=]+)=(\\s*\\S.*)$")};
+  regex equals{("^\\s*=\\s*$")};
+  regex equals_value{("^\\s*=(\\s*\\S.*$)")};
+
+  map.initialize();
+  string userinput;
+  getline(cin, userinput);
+  int linenumber = 0;
+  char eot = 4;
+  string end = "" + eot;
+  while (userinput != end){
+        line = userinput;
+        linenumber++;
+        cout << "-: " << linenumber << ": " << line << endl;
+        if(regex_search(line,match,comment)){
+               // continue;
+        }
+        if(regex_search(line,match,key)){
+               arg = match[0];
+               arg = trimstring(arg);
+               if(map.find(arg) == map.end()){
+                        try{
+                        string error = arg;
+                        throw runtime_error(error + 
+                        ": key not found");
+                        }
+                        catch (runtime_error err)
+                        {cerr << err.what() << endl;}
+                       // continue;
+               }else{
+                        cout << map.find(arg)->first << " = "
+                        << map.find(arg)->second << endl;
+               }
+        }
+        if(regex_search(line,match,key_equals)){
+               arg = match[1];
+               arg = trimstring(arg);
+               if(map.find(arg) != map.end()){
+                        map.erase(map.find(arg));
+               }
+              // continue;
+        }
+        if(regex_search(line,match,key_equals_value)){
+                arg = match[1];
+                arg2 = match[2];
+                arg = trimstring(arg);
+                arg2 = trimstring(arg2);
+                cout << arg << " = " << arg2 << endl;
+                str_str_pair pair(arg,arg2);
+                map.insert(pair);
+               // continue;
+        }
+        if(regex_search(line,match,equals)){
+                map.print();
+               // continue;
+        }
+        if(regex_search(line,match,equals_value)){
+               arg = match[1];
+               arg = trimstring(arg);
+               map.print_value(arg);
+              // continue;
+        } 
+        getline(cin, userinput);
+   }
+   map.~listmap();
+
+  /* sys_info::execname (argv[0]);
+   scan_options (argc, argv);
+
+   str_str_map test;
+   for (char** argp = &argv[optind]; argp != &argv[argc]; ++argp) {
+      str_str_pair pair (*argp, to_string<int> (argp - argv));
+      cout << "Before insert: " << pair << endl;
+      test.insert (pair);
+   }
+
+   for (str_str_map::iterator itor = test.begin();
+        itor != test.end(); ++itor) {
+      cout << "During iteration: " << *itor << endl;
+   }
+
+   str_str_map::iterator itor = test.begin();
+   test.erase (itor);
+
+   cout << "EXIT_SUCCESS" << endl;*/
+   return EXIT_SUCCESS;
+}
+
