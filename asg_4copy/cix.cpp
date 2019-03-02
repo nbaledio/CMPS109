7c81f7bde9b993cd14792594ab0e34e9a6667a05
nbaledio <nbaledio@ucsc.edu>
2019-03-02 00:49:37 -0800

    Added test server directory

---
+++ asg_4copy/cix.cpp
@@ -0,0 +1,151 @@
+// $Id: cix.cpp,v 1.7 2019-02-07 15:14:37-08 - - $
+
+#include <iostream>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <sstream>
+#include <fstream>
+using namespace std;
+
+#include <libgen.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "protocol.h"
+#include "logstream.h"
+#include "sockets.h"
+
+logstream log (cout);
+struct cix_exit: public exception {};
+
+unordered_map<string,cix_command> command_map {
+   {"exit", cix_command::EXIT},
+   {"help", cix_command::HELP},
+   {"ls"  , cix_command::LS  },
+   {"get" , cix_command::GET },
+};
+
+static const string help = R"||(
+exit         - Exit the program.  Equivalent to EOF.
+get filename - Copy remote file to local host.
+help         - Print help summary.
+ls           - List names of files on remote server.
+put filename - Copy local file to remote host.
+rm filename  - Remove file from remote server.
+)||";
+
+//get command : In Progress
+void cix_get(client_socket& server, string filename){
+   cix_header header;
+   //strcpy(header.filename, filename.c_str()); <- Use snprintf
+   snprintf(header.filename,sizeof(header.filename),filename.c_str());
+   header.command = cix_command::GET;
+   log << "sending header " << header << endl;
+   send_packet (server, &header, sizeof header);
+   recv_packet (server, &header, sizeof header);
+   log << "received header " << header << endl;
+   if(header.command != cix_command::FILEOUT){
+       log << "sent GET, server did not return FILEOUT" << endl;
+       log << "server returned " << header << endl;
+   }else{
+      char* buffer = new char[header.nbytes];
+      recv_packet (server, buffer, header.nbytes);
+      log << "received " << header.nbytes << " bytes" << endl;
+      std::ofstream newFile(filename,std::ios::binary);
+      newFile.write(buffer,header.nbytes);
+      buffer[header.nbytes] = '\0';
+      delete[] buffer; 
+   }
+}
+
+//help command
+void cix_help() {
+   cout << help;
+}
+
+//ls command
+void cix_ls (client_socket& server) {
+   cix_header header;
+   header.command = cix_command::LS;
+   log << "sending header " << header << endl;
+   send_packet (server, &header, sizeof header);
+   recv_packet (server, &header, sizeof header);
+   log << "received header " << header << endl;
+   if (header.command != cix_command::LSOUT) {
+      log << "sent LS, server did not return LSOUT" << endl;
+      log << "server returned " << header << endl;
+   }else {
+      auto buffer = make_unique<char[]> (header.nbytes + 1);
+      recv_packet (server, buffer.get(), header.nbytes);
+      log << "received " << header.nbytes << " bytes" << endl;
+      buffer[header.nbytes] = '\0';
+      cout << buffer.get();
+   }
+}
+
+
+void usage() {
+   cerr << "Usage: " << log.execname() << " [host] [port]" << endl;
+   throw cix_exit();
+}
+
+int main (int argc, char** argv) {
+   log.execname (basename (argv[0]));
+   log << "starting" << endl;
+   vector<string> args (&argv[1], &argv[argc]);
+   if (args.size() > 2) usage();
+   string host = get_cix_server_host (args, 0);
+   in_port_t port = get_cix_server_port (args, 1);
+   log << to_string (hostinfo()) << endl;
+   try {
+      log << "connecting to " << host << " port " << port << endl;
+      client_socket server (host, port);
+      log << "connected to " << to_string (server) << endl;
+      for (;;) {
+         string line;
+         getline (cin, line);
+         if (cin.eof()) throw cix_exit();
+         log << "command " << line << endl;
+         //Tokenizes string (For filename inputs)
+         vector <string> tokens;
+         stringstream reader(line);
+         string token;
+         while(getline(reader, token, ' ')){
+            tokens.push_back(token);
+         } 
+         const auto& itor = command_map.find (tokens[0]);
+         cix_command cmd = itor == command_map.end()
+                         ? cix_command::ERROR : itor->second;
+         switch (cmd) {
+            case cix_command::EXIT:
+               if(tokens.size() != 1){goto defaultc;}
+               throw cix_exit();
+               break;
+            case cix_command::HELP:
+               if(tokens.size() != 1){goto defaultc;}
+               cix_help();
+               break;
+            case cix_command::LS:
+               if(tokens.size() != 1){goto defaultc;}
+               cix_ls (server);
+               break;
+            case cix_command::GET:
+               cix_get(server, tokens[1]);
+               break;  
+            default:
+               defaultc:
+               log << line << ": invalid command" << endl;
+               break;
+         }
+      }
+   }catch (socket_error& error) {
+      log << error.what() << endl;
+   }catch (cix_exit& error) {
+      log << "caught cix_exit" << endl;
+   }
+   log << "finishing" << endl;
+   return 0;
+}
+
---
+++ asg_4copy/cixd.cpp
@@ -0,0 +1,194 @@
+// $Id: cixd.cpp,v 1.7 2016-05-09 16:01:56-07 - - $
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+#include <libgen.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "protocol.h"
+#include "logstream.h"
+#include "sockets.h"
+
+logstream log (cout);
+struct cix_exit: public exception {};
+
+void reply_ls (accepted_socket& client_sock, cix_header& header) {
+   const char* ls_cmd = "ls -l 2>&1";
+   FILE* ls_pipe = popen (ls_cmd, "r");
+   if (ls_pipe == NULL) { 
+      log << "ls -l: popen failed: " << strerror (errno) << endl;
+      header.command = cix_command::NAK;
+      header.nbytes = errno;
+      send_packet (client_sock, &header, sizeof header);
+      return;
+   }
+   string ls_output;
+   char buffer[0x1000];
+   for (;;) {
+      char* rc = fgets (buffer, sizeof buffer, ls_pipe);
+      if (rc == nullptr) break;
+      ls_output.append (buffer);
+   }
+   int status = pclose (ls_pipe);
+   if (status < 0) log << ls_cmd << ": " << strerror (errno) << endl;
+              else log << ls_cmd << ": exit " << (status >> 8)
+                       << " signal " << (status & 0x7F)
+                       << " core " << (status >> 7 & 1) << endl;
+   header.command = cix_command::LSOUT;
+   header.nbytes = ls_output.size();
+   memset (header.filename, 0, FILENAME_SIZE);
+   log << "sending header " << header << endl;
+   send_packet (client_sock, &header, sizeof header);
+   send_packet (client_sock, ls_output.c_str(), ls_output.size());
+   log << "sent " << ls_output.size() << " bytes" << endl;
+}
+
+//get reply function
+void reply_get(accepted_socket& client_sock, cix_header& header){
+  std::ifstream file(header.filename, std::ios::binary);
+  //If file is not found
+  if(file.is_open() == false){
+       log << "get: " << header.filename << ": " 
+       << strerror (errno) << endl;
+       header.command = cix_command::NAK;
+       send_packet (client_sock, &header, sizeof header);
+       header.nbytes = errno;
+       return;
+  }
+       file.seekg (0, file.end);
+       int length = file.tellg();
+       file.seekg(0, file.beg);
+       char * buffer = new char[length];
+       file.read(buffer, length);
+       header.command = cix_command::FILEOUT;
+       header.nbytes = length;
+       log << "sending header " << header << endl;
+       send_packet (client_sock, &header, sizeof header);
+       send_packet (client_sock, buffer, length);
+       log << "sent " << length << " bytes" << endl;    
+}
+
+
+void run_server (accepted_socket& client_sock) {
+   log.execname (log.execname() + "-server");
+   log << "connected to " << to_string (client_sock) << endl;
+   try {   
+      for (;;) {
+         cix_header header; 
+         recv_packet (client_sock, &header, sizeof header);
+         log << "received header " << header << endl;
+         switch (header.command) {
+            case cix_command::LS: 
+               reply_ls (client_sock, header);
+               break;
+            case cix_command::GET:
+               reply_get(client_sock, header);
+               break;
+            default:
+               log << "invalid header from client:" << header << endl;
+               break;
+         }
+      }
+   }catch (socket_error& error) {
+      log << error.what() << endl;
+   }catch (cix_exit& error) {
+      log << "caught cix_exit" << endl;
+   }
+   log << "finishing" << endl;
+   throw cix_exit();
+}
+
+void fork_cixserver (server_socket& server, accepted_socket& accept) {
+   pid_t pid = fork();
+   if (pid == 0) { // child
+      server.close();
+      run_server (accept);
+      throw cix_exit();
+   }else {
+      accept.close();
+      if (pid < 0) {
+         log << "fork failed: " << strerror (errno) << endl;
+      }else {
+         log << "forked cixserver pid " << pid << endl;
+      }
+   }
+}
+
+
+void reap_zombies() {
+   for (;;) {
+      int status;
+      pid_t child = waitpid (-1, &status, WNOHANG);
+      if (child <= 0) break;
+      log << "child " << child
+          << " exit " << (status >> 8)
+          << " signal " << (status & 0x7F)
+          << " core " << (status >> 7 & 1) << endl;
+   }
+}
+
+void signal_handler (int signal) {
+   log << "signal_handler: caught " << strsignal (signal) << endl;
+   reap_zombies();
+}
+
+void signal_action (int signal, void (*handler) (int)) {
+   struct sigaction action;
+   action.sa_handler = handler;
+   sigfillset (&action.sa_mask);
+   action.sa_flags = 0;
+   int rc = sigaction (signal, &action, nullptr);
+   if (rc < 0) log << "sigaction " << strsignal (signal) << " failed: "
+                   << strerror (errno) << endl;
+}
+
+
+int main (int argc, char** argv) {
+   log.execname (basename (argv[0]));
+   log << "starting" << endl;
+   vector<string> args (&argv[1], &argv[argc]);
+   signal_action (SIGCHLD, signal_handler);
+   in_port_t port = get_cix_server_port (args, 0);
+   try {
+      server_socket listener (port);
+      for (;;) {
+         log << to_string (hostinfo()) << " accepting port "
+             << to_string (port) << endl;
+         accepted_socket client_sock;
+         for (;;) {
+            try {
+               listener.accept (client_sock);
+               break;
+            }catch (socket_sys_error& error) {
+               switch (error.sys_errno) {
+                  case EINTR:
+                     log << "listener.accept caught "
+                         << strerror (EINTR) << endl;
+                     break;
+                  default:
+                     throw;
+               }
+            }
+         }
+         log << "accepted " << to_string (client_sock) << endl;
+         try {
+            fork_cixserver (listener, client_sock);
+            reap_zombies();
+         }catch (socket_error& error) {
+            log << error.what() << endl;
+         }
+      }
+   }catch (socket_error& error) {
+      log << error.what() << endl;
+   }catch (cix_exit& error) {
+      log << "caught cix_exit" << endl;
+   }
+   log << "finishing" << endl;
+   return 0;
+}
+
