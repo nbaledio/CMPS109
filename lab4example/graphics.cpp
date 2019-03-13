97afa1d747a4ee3121ae266862f2ccc8f8388322
nbaledio <nbaledio@ucsc.edu>
2019-03-13 16:30:37 -0700

    Test graphics

--- lab4example/graphics.cpp
+++ lab4example/graphics.cpp
@@ -1,4 +1,4 @@
-// $Id: graphics.cpp,v 1.3 2016-07-20 21:33:16-07 - - $
+// $Id: graphics.cpp,v 1.4 2019-02-28 15:24:20-08 - - $
 
 #include <iostream>
 using namespace std;
@@ -10,14 +10,8 @@ using namespace std;
 
 int window::width = 640; // in pixels
 int window::height = 480; // in pixels
-int window_dimensions::window_height = window::height;
-int window_dimensions::window_width = window::width;
-int window_dimensions::pixels_move = window::pixels_to_move_by;
-rgbcolor window_dimensions::border_color {255,   0,   0};
 vector<object> window::objects;
 size_t window::selected_obj = 0;
-int window::pixels_to_move_by = 4;
-int window::border_width = 4;
 mouse window::mus;
 
 // Executed when window system signals to shut down.
@@ -41,22 +35,8 @@ void window::entry (int mouse_entered) {
 // Called to display the objects in the window.
 void window::display() {
    glClear (GL_COLOR_BUFFER_BIT);
-   int i = 0;
-   string number;
-   vertex bounds_vertex;
-   //NEEDS TO BE HERE FOR TEXT OBJECT BORDER TO RENDER
-   objects[selected_obj].draw();
-   //Sets the border around current selected object
-   objects[selected_obj].draw_border();
-   //cout << "Drawing selected object's border " << endl;
-   for (auto& object: window::objects) {
-      bounds_vertex = object.get_center();
-      object.draw();
-      number = std::to_string(i);
-      object.set_object_number(number);
-      object.draw_object_number();
-      ++i;
-   }
+   for (auto& object: window::objects) object.draw();
+   mus.draw();
    glutSwapBuffers();
 }
 
@@ -65,8 +45,6 @@ void window::reshape (int width, int height) {
    DEBUGF ('g', "width=" << width << ", height=" << height);
    window::width = width;
    window::height = height;
-   window_dimensions::window_width = width;
-   window_dimensions::window_height = height;
    glMatrixMode (GL_PROJECTION);
    glLoadIdentity();
    gluOrtho2D (0, window::width, 0, window::height);
@@ -76,302 +54,65 @@ void window::reshape (int width, int height) {
    glutPostRedisplay();
 }
 
+
 // Executed when a regular keyboard key is pressed.
 void window::keyboard (GLubyte key, int x, int y) {
-
-   //cout << "moving by: " << pixels_to_move_by << " pixels" << endl;
-   //objects[selected_obj].draw_border();
    enum {BS = 8, TAB = 9, ESC = 27, SPACE = 32, DEL = 127};
    DEBUGF ('g', "key=" << unsigned (key) << ", x=" << x << ", y=" << y);
    window::mus.set (x, y);
    switch (key) {
       case 'Q': case 'q': case ESC:
-            window::close();
-            break;
+         window::close();
+         break;
       case 'H': case 'h':
-         {
-            objects[selected_obj].move(-1 * pixels_to_move_by, 0.0);
-            //objects[selected_obj].draw_border();
-            break;
-         }
+         //move_selected_object (
+         break;
       case 'J': case 'j':
-         {
-            objects[selected_obj].move(0.0, -1 * pixels_to_move_by);
-            //objects[selected_obj].draw_border();
-            break;
-         }
+         //move_selected_object (
+         break;
       case 'K': case 'k':
-         {
-            objects[selected_obj].move(0.0, pixels_to_move_by);
-            //objects[selected_obj].draw_border();
-            break;
-         }
+         //move_selected_object (
+         break;
       case 'L': case 'l':
-         {
-            objects[selected_obj].move(pixels_to_move_by, 0.0);
-            break;
-         }
+         //move_selected_object (
+         break;
       case 'N': case 'n': case SPACE: case TAB:
-         {
-            ++selected_obj;
-            if (selected_obj == objects.size())
-               selected_obj = 0;
-            break;
-         }
+         break;
       case 'P': case 'p': case BS:
-         {
-            unsigned int check = -1;
-            --selected_obj;
-            if (selected_obj == check)
-               selected_obj = objects.size() - 1;
-            break;
-         }
-      case '0'...'9':
-         {
-            size_t temp_selected_obj = key - '0';
-            //cout << "setting control to index: " << temp_selected_obj 
-            //<< endl << "objects size: " << objects.size() << endl;
-           
-            if(temp_selected_obj < objects.size())
-            {
-               selected_obj = temp_selected_obj;
-            }
-            else
-            {
-               cerr << unsigned (key - '0') 
-               << ": invalid index selection" << endl;
-            }
-            break;
-         }
+         break;
+      case '0': case '1': case '2': case '3': case '4':
+      case '5': case '6': case '7': case '8': case '9':
+         //select_object (key - '0');
+         break;
       default:
          cerr << unsigned (key) << ": invalid keystroke" << endl;
          break;
    }
-   //cout << "refreshing screen" << endl;
-   glClear(GL_DEPTH_BUFFER_BIT); 
    glutPostRedisplay();
 }
 
+
 // Executed when a special function key is pressed.
 void window::special (int key, int x, int y) {
    DEBUGF ('g', "key=" << key << ", x=" << x << ", y=" << y);
    window::mus.set (x, y);
    switch (key) {
       case GLUT_KEY_LEFT: //move_selected_object (-1, 0); break;
-         {
-            objects[selected_obj].move(-1 * pixels_to_move_by, 0.0);
-            //objects[selected_obj].draw_border();
-            break;
-         }
       case GLUT_KEY_DOWN: //move_selected_object (0, -1); break;
-         {
-            objects[selected_obj].move(0.0, -1 * pixels_to_move_by);
-            //objects[selected_obj].draw_border();
-            break;
-         }
       case GLUT_KEY_UP: //move_selected_object (0, +1); break;
-         {
-            objects[selected_obj].move(0.0, pixels_to_move_by);
-            //objects[selected_obj].draw_border();
-            break;
-         }
       case GLUT_KEY_RIGHT: //move_selected_object (+1, 0); break;
-         {
-            objects[selected_obj].move(pixels_to_move_by, 0.0);
-            break;
-         }
       case GLUT_KEY_F1: //select_object (1); break;
-         {
-            size_t temp_selected_obj = 1;
-            //cout << "setting control to index: " << temp_selected_obj 
-            //<< endl << "objects size: " << objects.size() << endl;
-           
-            if(temp_selected_obj <= objects.size())
-            {
-               selected_obj = 0;
-            }
-            else
-            {
-               cerr << "Error: invalid index selection" << endl;
-            }           
-            break;
-         }
       case GLUT_KEY_F2: //select_object (2); break;
-         {
-            size_t temp_selected_obj = 2;
-            //cout << "setting control to index: " << temp_selected_obj 
-            //<< endl << "objects size: " << objects.size() << endl;
-           
-            if(temp_selected_obj <= objects.size())
-            {
-               selected_obj = 1;
-            }
-            else
-            {
-               cerr << "Error: invalid index selection" << endl;
-            }           
-            break;
-         }
       case GLUT_KEY_F3: //select_object (3); break;
-         {
-            size_t temp_selected_obj = 3;
-            //cout << "setting control to index: " << temp_selected_obj 
-            //<< endl << "objects size: " << objects.size() << endl;
-           
-            if(temp_selected_obj <= objects.size())
-            {
-               selected_obj = 2;
-            }
-            else
-            {
-               cerr << "Error: invalid index selection" << endl;
-            }           
-            break;
-         }
       case GLUT_KEY_F4: //select_object (4); break;
-         {
-            size_t temp_selected_obj = 4;
-            //cout << "setting control to index: " << temp_selected_obj 
-            //<< endl << "objects size: " << objects.size() << endl;
-           
-            if(temp_selected_obj <= objects.size())
-            {
-               selected_obj = 3;
-            }
-            else
-            {
-               cerr << "Error: invalid index selection" << endl;
-            }           
-            break;
-         }
       case GLUT_KEY_F5: //select_object (5); break;
-         {
-            size_t temp_selected_obj = 5;
-            //cout << "setting control to index: " << temp_selected_obj 
-            //<< endl << "objects size: " << objects.size() << endl;
-           
-            if(temp_selected_obj <= objects.size())
-            {
-               selected_obj = 4;
-            }
-            else
-            {
-               cerr << "Error: invalid index selection" << endl;
-            }           
-            break;
-         }
       case GLUT_KEY_F6: //select_object (6); break;
-         {
-            size_t temp_selected_obj = 6;
-            //cout << "setting control to index: " << temp_selected_obj 
-            //<< endl << "objects size: " << objects.size() << endl;
-           
-            if(temp_selected_obj <= objects.size())
-            {
-               selected_obj = 5;
-            }
-            else
-            {
-               cerr << "Error: invalid index selection" << endl;
-            }           
-            break;
-         }
       case GLUT_KEY_F7: //select_object (7); break;
-         {
-            size_t temp_selected_obj = 7;
-            //cout << "setting control to index: " << temp_selected_obj 
-            //<< endl << "objects size: " << objects.size() << endl;
-           
-            if(temp_selected_obj <= objects.size())
-            {
-               selected_obj = 6;
-            }
-            else
-            {
-               cerr << "Error: invalid index selection" << endl;
-            }           
-            break;
-         }
       case GLUT_KEY_F8: //select_object (8); break;
-         {
-            size_t temp_selected_obj = 8;
-            //cout << "setting control to index: " << temp_selected_obj 
-            //<< endl << "objects size: " << objects.size() << endl;
-           
-            if(temp_selected_obj <= objects.size())
-            {
-               selected_obj = 7;
-            }
-            else
-            {
-               cerr << "Error: invalid index selection" << endl;
-            }           
-            break;
-         }
       case GLUT_KEY_F9: //select_object (9); break;
-         {
-            size_t temp_selected_obj = 9;
-            //cout << "setting control to index: " << temp_selected_obj 
-            //<< endl << "objects size: " << objects.size() << endl;
-           
-            if(temp_selected_obj <= objects.size())
-            {
-               selected_obj = 8;
-            }
-            else
-            {
-               cerr << "Error: invalid index selection" << endl;
-            }           
-            break;
-         }
       case GLUT_KEY_F10: //select_object (10); break;
-         {
-            size_t temp_selected_obj = 10;
-            //cout << "setting control to index: " << temp_selected_obj 
-            //<< endl << "objects size: " << objects.size() << endl;
-           
-            if(temp_selected_obj <= objects.size())
-            {
-               selected_obj = 9;
-            }
-            else
-            {
-               cerr << "Error: invalid index selection" << endl;
-            }           
-            break;
-         }
       case GLUT_KEY_F11: //select_object (11); break;
-         {
-            size_t temp_selected_obj = 11;
-            //cout << "setting control to index: " << temp_selected_obj 
-            //<< endl << "objects size: " << objects.size() << endl;
-           
-            if(temp_selected_obj <= objects.size())
-            {
-               selected_obj = 10;
-            }
-            else
-            {
-               cerr << "Error: invalid index selection" << endl;
-            }           
-            break;
-         }
       case GLUT_KEY_F12: //select_object (12); break;
-         {
-            size_t temp_selected_obj = 12;
-            //cout << "setting control to index: " << temp_selected_obj 
-            //<< endl << "objects size: " << objects.size() << endl;
-           
-            if(temp_selected_obj <= objects.size())
-            {
-               selected_obj = 11;
-            }
-            else
-            {
-               cerr << "Error: invalid index selection" << endl;
-            }           
-            break;
-         }
       default:
          cerr << unsigned (key) << ": invalid function key" << endl;
          break;
@@ -379,6 +120,7 @@ void window::special (int key, int x, int y) {
    glutPostRedisplay();
 }
 
+
 void window::motion (int x, int y) {
    DEBUGF ('g', "x=" << x << ", y=" << y);
    window::mus.set (x, y);
@@ -399,51 +141,28 @@ void window::mousefn (int button, int state, int x, int y) {
    glutPostRedisplay();
 }
 
-void window::set_new_center(string obj_name, vertex v) {
-
-   for (unsigned int num = 0; num < objects.size(); num++) {
-      
-      if (objects[num].get_name() == obj_name)
-      {
-         objects[num].set_new_center(v);
-      }
-  }
-}
-
-void window::set_color(string obj_name, const rgbcolor& color) {
-
-   for (unsigned int num = 0; num < objects.size(); num++) {
-      
-   if (objects[num].get_name() == obj_name)
-   {
-        objects[num].set_new_color(color);
-   }
-  }
-}
-
 void window::main () {
-   //cout << "CALLING WINDOW::MAIN" << endl;
    static int argc = 0;
    glutInit (&argc, nullptr);
    glutInitDisplayMode (GLUT_RGBA | GLUT_DOUBLE);
    glutInitWindowSize (window::width, window::height);
    glutInitWindowPosition (128, 128);
+   DEBUGF ('g', "glutCreateWindow (" << sys_info::execname() << ")");
    glutCreateWindow (sys_info::execname().c_str());
-   //glutCloseFunc (window::close);
-   //glutEntryFunc (window::entry);
-   glutSpecialFunc (window::special);
-   glutKeyboardFunc (window::keyboard);
+   glutCloseFunc (window::close);
+   glutEntryFunc (window::entry);
    glutDisplayFunc (window::display);
    glutReshapeFunc (window::reshape);
-   //glutKeyboardFunc (window::keyboard);
-   //glutSpecialFunc (window::special);
-   //glutMotionFunc (window::motion);
-   //glutPassiveMotionFunc (window::passivemotion);
-   //glutMouseFunc (window::mousefn);
+   glutKeyboardFunc (window::keyboard);
+   glutSpecialFunc (window::special);
+   glutMotionFunc (window::motion);
+   glutPassiveMotionFunc (window::passivemotion);
+   glutMouseFunc (window::mousefn);
    DEBUGF ('g', "Calling glutMainLoop()");
    glutMainLoop();
 }
 
+
 void mouse::state (int button, int state) {
    switch (button) {
       case GLUT_LEFT_BUTTON: left_state = state; break;
@@ -469,19 +188,3 @@ void mouse::draw() {
    }
 }
 
-void object::move (GLfloat delta_x, GLfloat delta_y) {
-   center.xpos += delta_x;
-   center.ypos += delta_y;
-   if (center.xpos == window_dimensions::window_width)
-      center.xpos = window_dimensions::pixels_move;
-   else if (center.xpos == 0) 
-      center.xpos = 
-         window_dimensions::window_width
-            - window_dimensions::pixels_move;
-   else if (center.ypos == window_dimensions::window_height) 
-      center.ypos = window_dimensions::pixels_move;
-   else if (center.ypos == 0)
-      center.ypos = 
-         window_dimensions::window_height
-            - window_dimensions::pixels_move;
-}
