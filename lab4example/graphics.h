86bf26d6faa87058cffdc704ab2f1d9d06234ef6
nbaledio <nbaledio@ucsc.edu>
2019-03-13 17:47:17 -0700

    Replacing example files

--- lab4example/graphics.h
+++ lab4example/graphics.h
@@ -11,19 +11,46 @@ using namespace std;
 
 #include "rgbcolor.h"
 #include "shape.h"
+#include "interp.h"
+
+struct window_dimensions {
+
+static int window_width;
+static int window_height;
+static int pixels_move;
+static rgbcolor border_color;
+
+};
 
 class object {
    private:
       shared_ptr<shape> pshape;
-      vertex center;
+      shared_ptr<shape> obj_number;
+      vertex center { 320, 240 };
       rgbcolor color;
+      rgbcolor border_color;
+      rgbcolor text_color {255, 255, 255};
+      string name;
    public:
-      // Default copiers, movers, dtor all OK.
-      void draw() { pshape->draw (center, color); }
-      void move (GLfloat delta_x, GLfloat delta_y) {
-         center.xpos += delta_x;
-         center.ypos += delta_y;
-      }
+      // Default copier, movers, dtor all OK.
+      object(string name_, shared_ptr<shape> ptr) 
+         : pshape(ptr), name(name_) {}
+      void set_new_center(vertex v) { 
+         center.xpos = v.xpos; center.ypos = v.ypos; }
+      vertex get_center() { return center; }
+      void set_new_color(const rgbcolor& input) { 
+         color = input; border_color = input; }
+      void set_object_number(string num) { 
+         obj_number = make_shared<text>("Fixed-8x13", num); }
+      void draw_object_number() { 
+         obj_number->draw(center, text_color); }
+      string get_name() { return name; }
+      void draw() { pshape->draw(center, color); }
+      void draw_border() { 
+         pshape->draw_border(center, 
+            window_dimensions::border_color); }
+      void clear_border() { border_color = color; }
+      void move (GLfloat delta_x, GLfloat delta_y);
 };
 
 class mouse {
@@ -41,12 +68,9 @@ class mouse {
       void draw();
 };
 
-
 class window {
       friend class mouse;
    private:
-      static int width;         // in pixels
-      static int height;        // in pixels
       static vector<object> objects;
       static size_t selected_obj;
       static mouse mus;
@@ -61,12 +85,18 @@ class window {
       static void passivemotion (int x, int y);
       static void mousefn (int button, int state, int x, int y);
    public:
+      static void set_new_center(string obj_name, vertex v);
+      static void set_color(string obj_name, const rgbcolor& color);
       static void push_back (const object& obj) {
                   objects.push_back (obj); }
       static void setwidth (int width_) { width = width_; }
       static void setheight (int height_) { height = height_; }
       static void main();
+      static int width;         // in pixels
+      static int height;        // in pixels
+      static int pixels_to_move_by;
+      static rgbcolor color_for_border;
+      static int border_width;
 };
 
 #endif
-
